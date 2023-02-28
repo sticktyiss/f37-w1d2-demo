920fce63409690224dc8e016c6f6b6f35a9d46b9
Tyiss Goodman <sticktyiss@gmail.com>
2023-02-28 15:41:59 -0700

    added demo file

---
+++ intro.js
@@ -0,0 +1,32 @@
+// console.log("Hello World")
+
+const firstName="Tyiss"
+
+//console.log(firstName)
+
+let age = 23
+
+const isCool = true
+
+//console.log(firstName, age, isCool)
+
+// console.log("Hello my name is " + firstName + " I am "+age+" years old.")
+
+// console.log(`Hello, my name is ${firstName} I am ${age} years old.`)
+
+let x = 4
+let y = 10
+let z = x + 8
+let sum = x + y + z
+
+// console.log(x)
+// console.log(y)
+// console.log(z) 
+
+// console.log(sum)
+
+if (x===y){
+    console.log('x and y are the same value')
+} else {
+    console.log('x and y are different values')
+}
